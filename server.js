a06c1ed294963edcb6b66a08a76e079282b5f8b2
Ibrahim BIDI <Zizwar@users.noreply.github.com>
2024-10-12 20:56:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -187,10 +187,9 @@ function buildFileStructure(entries) {
   return structure
 }
 
-// Start the server
 serve({
   fetch: app.fetch,
   port: 3001
 })
 
-console.log('Server is running on http://localhost:3001')
+console.log('Server is running on http://msn.so:3003')
